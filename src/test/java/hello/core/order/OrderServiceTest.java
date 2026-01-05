cb9429512dde5ed049b5cade4aaeef0c4708e74e
Seungki kim <ti9918@gmail.com>
2026-01-05 20:57:24 +0900

    26.01.05 Commit & Push ( refactoring and review, comment )

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -28,6 +28,6 @@ class OrderServiceTest {
         memberService.join(member);
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
-        System.out.println("order.getDiscountPrice() = " + order.getDiscountPrice());
+
     }
 }
